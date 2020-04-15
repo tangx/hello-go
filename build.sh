1bd56110e6b31ae5d0c10b1027dd8f549761cf3b
tangx <uyinn@live.com>
2020-04-15 10:15:24 +0800

    update: 更新 binary 名字

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ set -x
 TAG=${GITHUB_REF##*/}
 Machine=`uname -m`
 
-CGO_ENABLED=0 GOOS=linux go build -v -o bin/hello_${GOOS}_${Machine}_${TAG} .
+CGO_ENABLED=0 GOOS=darwin go build -v -o bin/hello_${GOOS}_${Machine}_${TAG} .
 CGO_ENABLED=0 GOOS=linux go build -v -o bin/hello_${GOOS}_${Machine}_${TAG} .
 
 
