f0f25b5c0861bfd6feb4559400e046d072485055
tangx <uyinn@live.com>
2020-04-15 16:16:47 +0800

    add draft and license

--- build.sh
+++ build.sh
@@ -6,7 +6,9 @@ set -x
 TAG=${GITHUB_REF##*/}
 Machine=`uname -m`
 
-BINARY=hello-go
+# BINARY=hello-go
+
+BINARY=${GITHUB_REPOSITORY##*/}     # GITHUB_REPOSITORY=tangx/hello-go -> hello-go
 
 CGO_ENABLED=0 GOOS=darwin go build -v -o bin/${BINARY}_Darwin_${Machine}_${TAG} .
 CGO_ENABLED=0 GOOS=linux go build -v -o bin/${BINARY}_Linux_${Machine}_${TAG} .
