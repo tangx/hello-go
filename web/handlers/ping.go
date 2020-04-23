bb129bfd44a554bff016c7d8e609bab1da1fe340
tangx <uyinn@live.com>
2020-04-24 00:09:14 +0800

    styles: add  handlers folder

---
+++ web/handlers/ping.go
@@ -0,0 +1,9 @@
+package handlers
+
+import "github.com/gin-gonic/gin"
+
+func PingPong(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "pong",
+	})
+}
--- web/server.go
+++ web/server.go
@@ -1,6 +1,9 @@
 package web
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/tangx/hello-go/web/handlers"
+)
 
 func NewServer() {
 
@@ -8,14 +11,7 @@ func NewServer() {
 
 	RouteV1 := r.Group("/v1")
 
-	RouteV1.GET("/ping", ping)
+	RouteV1.GET("/ping", handlers.PingPong)
 
 	r.Run()
-}
-
-func ping(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"message": "pong",
-	})
-
 }
