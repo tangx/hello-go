27652d1c38c473dce9595096bd56aa23cbaa3f1f
tangx <uyinn@live.com>
2020-04-23 23:47:25 +0800

    chore: nothing

---
+++ logger/logger.go
@@ -0,0 +1,19 @@
+package logger
+
+import (
+	"fmt"
+
+	"github.com/sirupsen/logrus"
+)
+
+func PrintLogLevel() {
+
+	logrus.Trace("logrus Trance = ", int(logrus.TraceLevel))
+	logrus.Debug("logrus debug = ", int(logrus.DebugLevel))
+	logrus.Info("logrus info = ", int(logrus.InfoLevel))
+	logrus.Warn("logrus Warn = ", int(logrus.WarnLevel))
+	fmt.Println("hello go")
+	logrus.Error("logrus error = ", int(logrus.ErrorLevel))
+	logrus.Fatal("logrus Fatal = ", int(logrus.FatalLevel))
+	logrus.Panic("logrus Panic = ", int(logrus.PanicLevel))
+}
--- main.go
+++ main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/sirupsen/logrus"
 	"github.com/tangx/go-querystring/query"
+	"github.com/tangx/hello-go/logger"
 )
 
 func main() {
@@ -14,20 +13,9 @@ func main() {
 	p1 := Person{"zhang", 10}
 	users["zhang"] = p1
 
-	logger()
+	logger.PrintLogLevel()
 }
 
-func logger() {
-
-	logrus.Trace("logrus Trance = ", int(logrus.TraceLevel))
-	logrus.Debug("logrus debug = ", int(logrus.DebugLevel))
-	logrus.Info("logrus info = ", int(logrus.InfoLevel))
-	logrus.Warn("logrus Warn = ", int(logrus.WarnLevel))
-	fmt.Println("hello go")
-	logrus.Error("logrus error = ", int(logrus.ErrorLevel))
-	logrus.Fatal("logrus Fatal = ", int(logrus.FatalLevel))
-	logrus.Panic("logrus Panic = ", int(logrus.PanicLevel))
-}
 func Greet(who string) {
 	logrus.Infof("Hello, %s", who)
 
